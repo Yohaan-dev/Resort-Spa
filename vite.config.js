e00ef6b1efa090cd346b6feaab1aee3ebe8666a0
Yohaan <yohanakipruto@gmail.com>
2025-12-10 15:32:37 +0300

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: '/Resort-Spa',
-  build: {
-    outDir: 'dist',
-  },
   server: {
     port: 3000,
   },
